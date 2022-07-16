313ac24d4b831021c26ca5239dda4bb74b191d5d
lakshy1279 <46641127+lakshy1279@users.noreply.github.com>
2022-07-16 14:37:45 +0530

    Update FirstMissingPositive.cpp

--- FirstMissingPositive.cpp
+++ FirstMissingPositive.cpp
@@ -1,4 +1,6 @@
 
+        
+//in this we swap the element with its index position if we not able to find the element corresponding to the index then this index is our ans.
         int n = nums.size() ;
         unordered_map<int,int>index ;
         int j = 1 ;
@@ -23,5 +25,18 @@
                 }
             }
         }
-        
         return n + 1 ;
+
+..O(1) space solution 
+int n=nums.size();
+        for( int i=0;i<n;i++ )
+            while(nums[i]>0&&nums[i]<=n&&nums[i]!=nums[nums[i]-1]) --checking whether element exist within the range and it is not in the correct position.
+                swap(nums[i],nums[nums[i]-1]);
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==i+1)
+                continue;
+             else
+                 return i+1;
+        }
+        return n+1;
