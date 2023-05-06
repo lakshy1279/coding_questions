d87717f0ce7c8de2d97564f174e442427fde0ad1
lakshy1279 <46641127+lakshy1279@users.noreply.github.com>
2023-05-06 17:15:33 +0530

    good question of two pointer

---
+++ Two Pointers/No_of_subs.cpp
@@ -0,0 +1,50 @@
+// You are given an array of integers nums and an integer target.
+
+// Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+
+
+ int getEle(vector<int>& nums, int ele, int l)
+    {
+        int r = nums.size() - 1;
+        while(l <= r)
+        {
+            int mid = (l + r) / 2;
+            if(nums[mid] > ele)
+            {
+                r = mid - 1;
+            }
+            else
+            l = mid + 1;
+        }
+        return l;
+    }
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int mod = 1000000007;
+        long long ans = 0;
+        int n = nums.size();
+        int power[n];
+        power[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            power[i] = (power[i - 1] * 2) % mod;
+        }
+        // we can also solve this without binary search using only two pointer.
+        // if(nums[i] + nums[j] > target) then j is of no use so we do j--
+        // if(nums[i] + nums[j] <= targte) in this case we can make subsequece with i as the min_ele 2^(j - i) and then do i++;
+        for(int i = 0; i < nums.size(); i++)
+        {
+           int value = target - nums[i];
+           if(value <= 0)
+           continue;
+           int upper1 = getEle(nums, value, i) - 1;
+           cout<<upper1 <<" "<< i<<endl;
+          if(upper1 >= i)
+          {
+           int ele = upper1 - i;
+           ans += power[ele];
+           ans %= mod;
+          }
+        }
+        return ans;
+    }
+};
