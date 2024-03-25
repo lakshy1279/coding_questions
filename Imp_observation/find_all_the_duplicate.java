9e7c002f9e40b200246c286269cdf3bfc84003e2
lakshy1279 <luckyron1279@gmail.com>
2024-03-25 11:33:27 +0530

    ques

---
+++ Imp_observation/find_all_the_duplicate.java
@@ -0,0 +1,31 @@
+
+
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+// You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [2,3]
+
+
+
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        // negagation technique
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums[Math.abs(nums[i]) - 1] = -1 * nums[Math.abs(nums[i]) - 1];
+            if(nums[Math.abs(nums[i]) - 1] > 0)
+            {
+                ans.add(Math.abs(nums[i]));
+            }
+        }
+        return ans;
+    }
+}
